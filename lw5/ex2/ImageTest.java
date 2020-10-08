b6b791ab2b404e8e48064059c20f7f38e3fdfccb
Krutix <Krutihine@yandex.ru>
2020-10-08 11:05:02 +0300

    [lw5] when image dont exist - close programm

--- lw5/ex2/ImageTest.java
+++ lw5/ex2/ImageTest.java
@@ -1,14 +1,14 @@
 package lw5.ex2;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
 public class ImageTest {
     public static void main(String[] args) throws IOException {
         if (args.length < 1)
+            return ;
+        File f = new File(args[0]);
+        if(!f.exists() || f.isDirectory())
             return ;
         System.out.println(args[0]);
         ImageWindow fr = new ImageWindow(args[0]);
