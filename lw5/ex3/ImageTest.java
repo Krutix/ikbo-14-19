af234cd769c2a573444d21423ae543b6821f529a
Krutix <Krutihine@yandex.ru>
2020-10-08 15:02:47 +0300

    TODO

--- lw5/ex3/ImageTest.java
+++ lw5/ex3/ImageTest.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 
 public class ImageTest {
     public static void main(String[] args) throws IOException {
-        ImageWindow fr = new ImageWindow();
+        ImageWindow fr = new ImageWindow(); //TODO not working!!!
     }
 }
