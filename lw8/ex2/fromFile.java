eb4a8b8bd42db35e93cea8524866057ae117653f
Krutix <Krutihine@yandex.ru>
2020-10-09 00:06:47 +0300

    [lw8] read from file

--- lw8/ex2/fromFile.java
+++ lw8/ex2/fromFile.java
@@ -1,17 +1,18 @@
-package lw8.ex1;
+package lw8.ex2;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class toFile {
+public class fromFile {
     public static void main(String[] args) throws IOException {
-        char[] buff = new char[1024];
+        char[] buff = new char[100];
         int red;
         FileReader reader = new FileReader("ConsoleRead.txt");
-        while ((red = reader.read(buff)) != 0 && red != -1)
-            for (int i = 0; i < red; i++)
-                System.out.print(buff[i]);
+        while ((red = reader.read(buff)) != 0)
+        {
+            System.out.print(buff);
+        }
         reader.close();
     }
 }
