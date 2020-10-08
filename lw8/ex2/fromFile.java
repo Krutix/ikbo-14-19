3e34cf10669a79f7febacdcd21886699d026bc85
Krutix <Krutihine@yandex.ru>
2020-10-09 00:11:34 +0300

    [lw8] ex1 fix and ex3

--- lw8/ex2/fromFile.java
+++ lw8/ex2/fromFile.java
@@ -2,14 +2,13 @@ package lw8.ex2;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class fromFile {
     public static void main(String[] args) throws IOException {
-        char[] buff = new char[100];
+        char[] buff = new char[1024];
         int red;
         FileReader reader = new FileReader("ConsoleRead.txt");
-        while ((red = reader.read(buff)) != 0)
+        while ((red = reader.read(buff)) != 0 && red != -1)
         {
             System.out.print(buff);
         }
--- lw8/ex3/toFile.java
+++ lw8/ex3/toFile.java
@@ -1,4 +1,4 @@
-package lw8.ex1;
+package lw8.ex3;
 
 import java.io.FileWriter;
 import java.io.IOException;
