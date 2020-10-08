ceef0dacc9a71c03c97fc572fe491edff850114e
Krutix <Krutihine@yandex.ru>
2020-10-08 19:11:07 +0300

    [lw5] ex3 done

--- lw5/ex3/ImageWindow.java
+++ lw5/ex3/ImageWindow.java
@@ -1,9 +1,11 @@
 package lw5.ex3;
 
-import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.imageio.ImageIO;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -13,44 +15,46 @@ public class ImageWindow extends JPanel {
     private JFrame frame;
     private Timer timer;
     private Image image;
+    private int count;
 
     public ImageWindow() {
         frame = new JFrame("Coin");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(200, 200);
+        frame.setSize(217, 235);
         frame.add(this);
+        frame.setResizable(false);
         frame.setVisible(true);
-        image = new ImageIcon("anim/coin0.png").getImage();
+        image = new ImageIcon("src\\lw5\\ex3\\anim\\coin0.png").getImage();
+        repaint();
         count = 0;
         timer = new Timer();
         timer.schedule(animate, 1, 200);
     }
 
-    private int count;
     TimerTask animate = new TimerTask() {
         @Override
         public void run() {
             switch (count) {
                 case 0:
-                    image = new ImageIcon("anim/coin0.png").getImage();
+                    image = new ImageIcon("src\\lw5\\ex3\\anim\\coin0.png").getImage();
                     break;
                 case 1:
-                    image = new ImageIcon("anim/coin1.png").getImage();
+                    image = new ImageIcon("src\\lw5\\ex3\\anim\\coin1.png").getImage();
                     break;
                 case 2:
-                    image = new ImageIcon("anim/coin2.png").getImage();
+                    image = new ImageIcon("src\\lw5\\ex3\\anim\\coin2.png").getImage();
                     break;
                 case 3:
-                    image = new ImageIcon("anim/coin3.png").getImage();
+                    image = new ImageIcon("src\\lw5\\ex3\\anim\\coin3.png").getImage();
                     break;
                 case 4:
-                    image = new ImageIcon("anim/coin4.png").getImage();
+                    image = new ImageIcon("src\\lw5\\ex3\\anim\\coin4.png").getImage();
                     break;
                 case 5:
-                    image = new ImageIcon("anim/coin5.png").getImage();
+                    image = new ImageIcon("src\\lw5\\ex3\\anim\\coin5.png").getImage();
                     break;
                 case 6:
-                    image = new ImageIcon("anim/coin6.png").getImage();
+                    image = new ImageIcon("src\\lw5\\ex3\\anim\\coin6.png").getImage();
                     break;
                 default:
                     break;
@@ -65,6 +69,7 @@ public class ImageWindow extends JPanel {
 
     @Override
     public void paint(Graphics canvas) {
+        super.paint(canvas);
         canvas.drawImage(image, 0, 0,  null);
     }
 }
