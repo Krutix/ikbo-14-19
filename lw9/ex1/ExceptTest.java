6b124ef496b2702dba731b8b4d0f8a1fe5943b1c
Krutix <Krutihine@yandex.ru>
2020-10-09 12:31:04 +0300

    [lw9] ex1-4

---
+++ lw9/ex1/ExceptTest.java
@@ -0,0 +1,33 @@
+package lw9.ex1;
+
+import java.util.Scanner;
+
+public class ExceptTest {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner( System.in );
+        System.out.print("Enter an integer ");
+        while (true) {
+            String intString = myScanner.next();
+            try {
+                int i = Integer.parseInt(intString);
+                exceptionDemo(i);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Entered not a number");
+            }
+            catch (ArithmeticException e) {
+                System.out.println("Attempted division by zero");
+            }
+            catch (Exception e) {
+                System.out.println("Error");
+            }
+            finally {
+                System.out.print("Enter an integer ");
+            }
+        }
+    }
+
+    public static void exceptionDemo(int nb) {
+        System.out.println(2 / nb);
+    }
+}
