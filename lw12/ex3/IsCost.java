13271c40214095804709bb77f5438288f0618a51
Krutix <Krutihine@yandex.ru>
2020-10-12 13:51:08 +0300

    [lw12] is cost

---
+++ lw12/ex3/IsCost.java
@@ -0,0 +1,19 @@
+package lw12.ex3;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class IsCost {
+    private static boolean isRightCost(String costStr)
+    {
+        if (Pattern.matches("\\d*(.\\d\\d)? ((EU)|(USD)|(RUB))", costStr))
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine())
+            System.out.println(isRightCost(scanner.nextLine()));
+    }
+}
