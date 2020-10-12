f6981007e5c4fc2e9d45b5d02a23cd373b7c48e5
Krutix <Krutihine@yandex.ru>
2020-10-12 13:22:24 +0300

    [lw12] split with regex

--- lw12/ex1/Split.java
+++ lw12/ex1/Split.java
@@ -1,25 +1,20 @@
 package lw12.ex1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.regex.*;
 
 public class Split {
-    public static ArrayList<String> toArray(String toParse, String delim)
+    public static String[] toArray(String toParse, String delim)
     {
-        StringTokenizer st = new StringTokenizer(toParse, delim);
-        ArrayList<String> array = new ArrayList<>();
-        while(st.hasMoreTokens())
-            array.add(st.nextToken());
-        return array;
+        Pattern split = Pattern.compile(delim);
+        return split.split(toParse);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String toParse = scanner.nextLine();
         String delim = scanner.nextLine();
-        List<String> list = toArray(toParse, delim);
+        String[] list = toArray(toParse, delim);
         for (String elem : list)
             System.out.println(elem);
     }
